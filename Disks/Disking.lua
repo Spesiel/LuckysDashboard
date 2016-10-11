53eb772da57ccac1424793895b015bdad674eb5e
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-10-11 12:59:18 -0700

    Disks: First part of widget display

--- Disks/Disking.lua
+++ Disks/Disking.lua
@@ -1,16 +1,30 @@
 ------- Metadata --------------------------------------------------------------
 -- Disking
 -- Author: Lucky Penny
--- Description: Methods to simply the process of updating the preview of a
---              theme and applying it to the whole skin.
+-- Description: Calculates on the fly the correct height for the widget.
 -- License: Creative Commons BY-NC-SA 3.0
 -- Version: 0.0.1
 --
--- Initialize(): Generates the meters based on to the list of available themes. 
+-- Initialize(): As described. 
 -------------------------------------------------------------------------------
 
 
 -- Load all schemes from file
 function Initialize()
-    print("passed in disking Initialize")
+    local height = 0
+    local disksTotal = SKIN:GetVariable("DisksTotal")
+
+    height = disksTotal*36
+
+    for currentDisk=1,disksTotal,1 do
+        if SKIN:GetVariable("Disk"..currentDisk.."HideTemperature")=="0" then
+            height = height + 10
+        end
+        if SKIN:GetVariable("Disk"..currentDisk.."HideActivity")=="0" then
+            height = height + 20*2
+        end
+    end
+
+    SKIN:Bang("!SetOption SkinSizing H "..height*SKIN:GetVariable("ScaleDisks"))
+    SKIN:Bang("!UpdateMeter SkinSizing")
 end
